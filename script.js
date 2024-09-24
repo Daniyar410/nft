9bd6f402225634eddd0bf08fdbf65a75a0cc3254
Daniyar410 <ramziya.shakirova@gmail.com>
2024-09-24 16:51:19 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,69 @@
-const catalog = [
-    {
-        img: './img/Rectangle11.png',
-        text: '3D Soft Curves',
-        price: '0.85 ETH',
-        name:''
-    }
-]
+// const catalog = [
+//     {
+//         img: './img/Rectangle11.png',
+//         text: '3D Soft Curves',
+//         price: '0.85 ETH',
+//         name:''
+//     }
+// ]
+
+const btn = document.querySelector('#open-btn');
+const modal_close = document.querySelector('#unactive');
+const body = document.querySelector('body');
+const modal = document.querySelector('#modal');
+
+btn.onclick = () => {
+  modal.classList.add('active');
+  body.classList.add('scroll');
+};
+
+modal_close.onclick = () => {
+  modal.classList.remove('active');
+  body.classList.remove('scroll');
+};
+
+document
+  .getElementById('telegramForm')
+  .addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const phone = document.getElementById('phone').value;
+    const token = '7468903894:AAHQyBppSQtHNdyEYIsscNZs01X7d6fEVDI'; // Замените на ваш токен
+    const chat_id = '-4574918556'; // Замените на ваш chat_id
+    const url = `https://api.telegram.org/bot${token}/sendMessage`;
+
+    // Формируем сообщение
+    const message = `Name: ${name}\nPhone: ${phone}`;
+
+    const data = {
+      chat_id: chat_id,
+      text: message,
+    };
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.ok) {  
+          //   alert('Message sent!');
+
+          setTimeout(() => {
+            location.reload();
+          }, 3000);
+          const thanks = document.querySelector('#thanks');
+          thanks.innerText = 'спасибо';
+        } else {
+          alert('Error sending message.');
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        alert('Error sending message.');
+      });
+  });
