3b9f276fe0d97ec1ef0e940d767e6b034b063754
Daniyar410 <ramziya.shakirova@gmail.com>
2024-09-29 14:00:16 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -158,13 +158,13 @@ const catalog = [
     name: "Current Bid",
     price: "0.85 ETH",
   },
-  
 ];
 
 const wrap = document.querySelector(".wrap1");
 
 const cards = catalog.map((card) => {
-  return `
+  
+    return `
      <div class="card2">
               <img src=${card.img} alt="" />
   
@@ -183,7 +183,107 @@ const cards = catalog.map((card) => {
                 </div>
               </div>
             </div>
+
+            
   `;
-}).join('');
+  }).join("");
+
 
 wrap.innerHTML = cards;
+
+const catalogB = [
+  {
+    incon: "./img/card6.png",
+    avatarB: "./img/emotion2.jpg",
+    titelA: "Tung Tran",
+    titelB: "Bad Artist, NFT  Enthusiast, <br> Metaverse Innovator  Supporting #bitcoin since 2012",
+    click: "Follow",
+    follow: "3K Followers",
+  },
+  {
+    incon: "./img/card7.png",
+    avatarB: "./img/emotion2.jpg",
+    titelA: "Wade Warren",
+    titelB: "Bad Artist, NFT  Enthusiast, <br> Metaverse Innovator  Supporting #bitcoin since 2012",
+    click: "Follow",
+    follow: "8k Followers",
+  },
+  {
+    incon: "./img/card8.png",
+    avatarB: "./img/emotion4.png",
+    titelA: "Arlene McCoy",
+    titelB: "Bad Artist, NFT  Enthusiast, <br> Metaverse Innovator  Supporting #bitcoin since 2012",
+    click: "Follow",
+    follow: "650 Followers",
+  },
+  {
+    incon: "./img/card9.png",
+    avatarB: "./img/emotion6.png",
+    titelA: "Dianne Russell",
+    titelB: "Bad Artist, NFT  Enthusiast, <br> Metaverse Innovator  Supporting #bitcoin since 2012",
+    click: "Follow",
+    follow: "1K Followers",
+  },
+  {
+    incon: "./img/card6.png",
+    avatarB: "./img/emotion3.png",
+    titelA: "Savannah Nguyen",
+    titelB: "Bad Artist, NFT  Enthusiast, <br> Metaverse Innovator  Supporting #bitcoin since 2012",
+    click: "Follow",
+    follow: "238 Followers",
+  },
+  {
+    incon: "./img/card7.png",
+    avatarB: "./img/emotion3.png",
+    titelA: "Jenny Wilson",
+    titelB: "Bad Artist, NFT  Enthusiast, <br> Metaverse Innovator  Supporting #bitcoin since 2012",
+    click: "Follow",
+    follow: "64 Followers",
+  },
+  {
+    incon: "./img/card8.png",
+    avatarB: "./img/emotion4.png",
+    titelA: "Brooklyn Simmons",
+    titelB: "Bad Artist, NFT  Enthusiast, <br> Metaverse Innovator  Supporting #bitcoin since 2012",
+    click: "Follow",
+    follow: "2K Followers",
+  },
+  {
+    incon: "./img/card9.png",
+    avatarB: "./img/emotion2.jpg",
+    titelA: "Theresa Webb",
+    titelB: "Bad Artist, NFT  Enthusiast, <br> Metaverse Innovator  Supporting #bitcoin since 2012",
+    click: "Follow",
+    follow: "127 Followers",
+  },
+];
+
+
+const wrapB = document.querySelector('.wrap2')
+
+const cardsB = catalogB.map((cardB) => {
+
+  return `
+    <div class="card3">
+            <img src=${cardB.incon} alt="">
+
+            <div class="div2">
+              <div class="avatar">
+                <img src=${cardB.avatarB} alt="">
+              </div>
+              <div class="titel">
+                <h3>${cardB.titelA}</h3>
+                <p>${cardB.titelB}</p>
+
+                  <div class="follow">
+                    <h3>${cardB.follow}</h3>
+                    <button>${cardB.click}</button>
+                  </div>
+              </div>
+            </div>
+            
+          </div>
+  `
+}).join("")
+
+wrapB.innerHTML = cardsB
